a66abf908be49622d93b27908e603cb88a764bc3
IsmailZekry <IsmailZekry@users.noreply.github.com>
2021-06-08 00:17:41 +0200

    new ground

--- FlappyBirdClone/FlappyBirdClone/Main.cpp
+++ FlappyBirdClone/FlappyBirdClone/Main.cpp
@@ -7,15 +7,12 @@
 #pragma comment(lib, "freeimage.lib")
 
 
-GLuint BackgroundImage;
-GLuint bird;
-double birdy = 0;
-bool spaceKeyPressed = false;
+GLuint BackgroundImage, Ground;
+bool gameOver = false;
 
-int loadTexture(const char* location)
-{
+int loadTexture(const char* location) {
+
 	GLuint texture;
-
 
 	FIBITMAP* myImage = FreeImage_Load(FreeImage_GetFileType(location, 0), location);
 	myImage = FreeImage_ConvertTo32Bits(myImage);
@@ -38,60 +35,126 @@ int loadTexture(const char* location)
 
 }
 
+class BirdObject {
+public:
+
+	float y1, y2;
+	GLuint texture;
+	float groundLevelBird = -0.7;
+
+	void initializeBirdObject() {
+		texture = loadTexture("assets/images/bird0.png");
+		y1 = -0.06;
+		
+	}
+
+	void renderBird() {
+
+		y2 = y1 + (float) 0.12;
+		
+		if (!gameOver) {
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glEnable(GL_BLEND);
+
+			glBindTexture(GL_TEXTURE_2D, texture);
+			glPushMatrix();
+			glBegin(GL_QUADS);
+			glTexCoord2f(0, 0); glVertex2f(-0.06, y1); //Down Left
+			glTexCoord2f(1, 0); glVertex2f(0.06, y1); //Down Right
+			glTexCoord2f(1, 1); glVertex2f(0.06, y2); //Upper Left
+			glTexCoord2f(0, 1); glVertex2f(-0.06, y2); //Upper Right
+			glEnd();
+			glPopMatrix();
+
+			glDisable(GL_BLEND);
+		}
+	}
+
+	void update() {
+		if (y1 < groundLevelBird) {
+			gameOver = true;
+		}
+		else {
+			if (GetAsyncKeyState(VK_SPACE) != 0)
+				y1 += 0.03;
+			else
+				y1 -= 0.01;
+		}
+	}
+
+};
+
+BirdObject theBird;
+
+//class PipeObject {
+//	struct Pipe {
+//		float x, y;
+//		float speed;
+//		float lifetime;
+//		GLuint texture;
+//	};
+//
+//	Pipe* pipes;
+//	int count;
+//	float topLevel, GroundLevel;
+//	GLuint pipeTexture[2];
+//
+//public:
+//	PipeObject() {
+//		pipes = new Pipe[2];
+//	}
+//};
+
+//void handleKeyboard() {
+//
+//	if (GetAsyncKeyState(VK_SPACE) != 0)
+//		theBird.y1 += 0.03;
+//	else
+//		theBird.y1 -= 0.01;
+//}
+
+void myTimer(int t) {
+	//handleKeyboard();
+	theBird.update();
+	glutPostRedisplay();
+	glutTimerFunc(16, myTimer, 0);
+}
+
 void renderScene() {
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	
 	//Background Image
 	glBindTexture(GL_TEXTURE_2D, BackgroundImage);
 	glPushMatrix();
 	glBegin(GL_QUADS);
 	glTexCoord2f(0, 0); glVertex2f(-1, -1); //Down Left
-	glTexCoord2f(1, 0); glVertex2f( 1, -1); //Down Right
-	glTexCoord2f(1, 1); glVertex2f( 1,  1); //Upper Left
-	glTexCoord2f(0, 1); glVertex2f(-1,  1); //Upper Right
+	glTexCoord2f(1, 0); glVertex2f(1, -1); //Down Right
+	glTexCoord2f(1, 1); glVertex2f(1, 1); //Upper Left
+	glTexCoord2f(0, 1); glVertex2f(-1, 1); //Upper Right
 	glEnd();
 	glPopMatrix();
 
-	//Bird Image
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_BLEND);
-
-	glBindTexture(GL_TEXTURE_2D, bird);
+	//Ground
+	glBindTexture(GL_TEXTURE_2D, Ground);
 	glPushMatrix();
 	glBegin(GL_QUADS);
-	glTexCoord2f(0, 0); glVertex2f(-0.06, -0.06 + birdy); //Down Left
-	glTexCoord2f(1, 0); glVertex2f(0.06, -0.06 + birdy); //Down Right
-	glTexCoord2f(1, 1); glVertex2f(0.06, 0.06 + birdy); //Upper Left
-	glTexCoord2f(0, 1); glVertex2f(-0.06, 0.06 + birdy); //Upper Right
+	glTexCoord2f(0, 0); glVertex2f(-1, -1); //Down Left
+	glTexCoord2f(1, 0); glVertex2f(1, -1); //Down Right
+	glTexCoord2f(1, 1); glVertex2f(1, -0.7); //Upper Left
+	glTexCoord2f(0, 1); glVertex2f(-1, -0.7); //Upper Right
 	glEnd();
 	glPopMatrix();
 
-	glDisable(GL_BLEND);
+	//Render Bird
+	theBird.renderBird();
 
 
 	glutSwapBuffers();
 }
 
-void handleKeyboard() {
-
-	if (GetAsyncKeyState(VK_SPACE) != 0) {
-		birdy += 0.03;
-	}
-	else {
-		birdy -= 0.01;
-	}
-}
-
-void myTimer(int t)
-{
-	handleKeyboard();
-	glutPostRedisplay();
-	glutTimerFunc(16, myTimer, 0);
-}
-
 void main(int argc, char* argv[]) {
+	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(0,0);
@@ -100,11 +163,13 @@ void main(int argc, char* argv[]) {
 	glutCreateWindow("Flappy Bird Clone");
 
 	glutDisplayFunc(renderScene);
-	glutTimerFunc(16, myTimer, 0);
 
+	glutTimerFunc(16, myTimer, 0);
 	glEnable(GL_TEXTURE_2D);
 	BackgroundImage = loadTexture("assets/images/backgroundImage.png");
-	bird = loadTexture("assets/images/bird0.png");
+	Ground = loadTexture("assets/images/ground.png");
+
+	theBird.initializeBirdObject();
 
 	glutMainLoop();
 
