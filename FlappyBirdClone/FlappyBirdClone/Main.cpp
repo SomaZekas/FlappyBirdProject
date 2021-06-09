ac5bba4dd636bdd2a0220a6e5fcaf9d87fb59201
IsmailZekry <IsmailZekry@users.noreply.github.com>
2021-06-09 22:57:18 +0200

    9/6/2021

--- FlappyBirdClone/FlappyBirdClone/Main.cpp
+++ FlappyBirdClone/FlappyBirdClone/Main.cpp
@@ -7,7 +7,7 @@
 #pragma comment(lib, "freeimage.lib")
 
 
-GLuint BackgroundImage, Ground;
+GLuint BackgroundImage, Ground, Pipes;
 bool gameOver = false;
 
 int loadTexture(const char* location) {
@@ -35,6 +35,64 @@ int loadTexture(const char* location) {
 
 }
 
+
+class PipeObject {
+public:
+
+	struct Pipe {
+		float x1, x2;
+		float y1, y2;
+		GLuint texture;
+	};
+
+	Pipe* pipes;
+	int count;
+	float leftEdge = -1;
+	
+public:
+	PipeObject(int count) {
+		this->count = count;
+		pipes = new Pipe[count];
+	}
+
+	void renderPipes()
+	{
+		for (int i = 0; i < count; i++) {
+			
+			//will draw another pipe with rotation 90 degrees with same x but opposite y-axis, with random gap.
+			//on condition that two pipes appear on screen.
+
+			//Pipes
+			glBindTexture(GL_TEXTURE_2D, pipes[i].texture);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glPushMatrix();
+			glBegin(GL_QUADS);
+			glTexCoord2f(0, 0); glVertex2f(0.2, -0.7); //Down Left
+			glTexCoord2f(1, 0); glVertex2f(0.3, -0.7); //Down Right
+			glTexCoord2f(1, 1); glVertex2f(0.3, 0.0); //Upper Left
+			glTexCoord2f(0, 1); glVertex2f(0.2, 0.0); //Upper Right
+			glEnd();
+			glPopMatrix();
+
+			glDisable(GL_BLEND);
+		}
+	}
+
+	void initializePipeObject() {
+		for (int i = 0; i < count; i++)
+		{
+			pipes[i].texture = loadTexture("assets/images/pipe.png");
+			//t3rf 2l x bel initial value bta3o wa tzwd 2l i.
+		}
+		
+		
+	}
+
+
+};
+
+PipeObject thePipe(3);
+
 class BirdObject {
 public:
 
@@ -45,13 +103,13 @@ public:
 	void initializeBirdObject() {
 		texture = loadTexture("assets/images/bird0.png");
 		y1 = -0.06;
-		
+
 	}
 
 	void renderBird() {
 
-		y2 = y1 + (float) 0.12;
-		
+		y2 = y1 + (float)0.12;
+
 		if (!gameOver) {
 			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 			glEnable(GL_BLEND);
@@ -70,48 +128,33 @@ public:
 		}
 	}
 
+
 	void update() {
-		if (y1 < groundLevelBird) {
-			gameOver = true;
-		}
-		else {
+		//dimming when lost.
+		//rotate when up or down.
+
+		//if (y1 < groundLevelBird) {
+		//	gameOver = true;
+		//}
+		//else {
 			if (GetAsyncKeyState(VK_SPACE) != 0)
 				y1 += 0.03;
 			else
 				y1 -= 0.01;
-		}
+		//}
 	}
 
 };
 
 BirdObject theBird;
 
-//class PipeObject {
-//	struct Pipe {
-//		float x, y;
-//		float speed;
-//		float lifetime;
-//		GLuint texture;
-//	};
-//
-//	Pipe* pipes;
-//	int count;
-//	float topLevel, GroundLevel;
-//	GLuint pipeTexture[2];
-//
-//public:
-//	PipeObject() {
-//		pipes = new Pipe[2];
-//	}
-//};
+void handleKeyboard() {
 
-//void handleKeyboard() {
-//
-//	if (GetAsyncKeyState(VK_SPACE) != 0)
-//		theBird.y1 += 0.03;
-//	else
-//		theBird.y1 -= 0.01;
-//}
+	if (GetAsyncKeyState(VK_SPACE) != 0)
+		theBird.y1 += 0.03;
+	else
+		theBird.y1 -= 0.01;
+}
 
 void myTimer(int t) {
 	//handleKeyboard();
@@ -149,16 +192,34 @@ void renderScene() {
 	//Render Bird
 	theBird.renderBird();
 
+	//render Pipe
+	thePipe.renderPipes();
+
+	//Pipes
+	//glBindTexture(GL_TEXTURE_2D, Pipes);
+	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	//glPushMatrix();
+	//glBegin(GL_QUADS);
+	//glTexCoord2f(0, 0); glVertex2f(0.2, -0.7); //Down Left
+	//glTexCoord2f(1, 0); glVertex2f(0.3, -0.7); //Down Right
+	//glTexCoord2f(1, 1); glVertex2f(0.3, 0.0); //Upper Left
+	//glTexCoord2f(0, 1); glVertex2f(0.2, 0.0); //Upper Right
+	//glEnd();
+	//glPopMatrix();
+
+	//glDisable(GL_BLEND);
+
+
 
 	glutSwapBuffers();
 }
 
 void main(int argc, char* argv[]) {
-	
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(0,0);
-	glutInitWindowSize(600,700);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(600, 700);
 
 	glutCreateWindow("Flappy Bird Clone");
 
@@ -168,8 +229,9 @@ void main(int argc, char* argv[]) {
 	glEnable(GL_TEXTURE_2D);
 	BackgroundImage = loadTexture("assets/images/backgroundImage.png");
 	Ground = loadTexture("assets/images/ground.png");
-
 	theBird.initializeBirdObject();
+	//Pipes = loadTexture("assets/images/pipe.png");
+	thePipe.initializePipeObject();
 
 	glutMainLoop();
 
